d3051f64ae52c3a76aced216855ef56d6b65e324
Mewerton Melo <mewerton@gmail.com>
2023-01-26 16:42:32 -0300

    Update index.jsx

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -4,7 +4,8 @@ export function Details() {
   
   return (
     <Container>
-      <h1>Olá, Vamos iniciar!</h1>
+      <h1>Olá, Vamos iniciar o site do Hotel!</h1>
+      <h1>Paraíso Barra Hotel</h1>
     </Container>
   )
 }
